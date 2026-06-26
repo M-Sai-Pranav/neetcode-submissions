5d3026ed5825978400f986bcfe2d51fb094d8499
M-Sai-Pranav <94521881+M-Sai-Pranav@users.noreply.github.com>
2026-06-25 21:17:57 -0400

    Add: shortest-path-in-binary-matrix - submission-0

---
+++ Data Structures & Algorithms/shortest-path-in-binary-matrix/submission-0.py
@@ -0,0 +1,48 @@
+from collections import deque
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        ans = 0
+        if self.explore(grid, 0, 0, set(), ans) > 0:
+            return self.explore(grid, 0, 0, set(), ans)
+        return -1 
+    def inbounds(self, grid, r, c):
+        row = 0 <= r < len(grid)
+        col = 0 <= c < len(grid[0])
+        return row and col
+    def explore(self, grid, r, c, visited, ans):
+        if grid[r][c] == 1:
+            return -1
+        visited.add((r,c))
+        queue = deque([(r,c,1)])
+
+        while queue:
+            r,c,d = queue.popleft()
+            if r == len(grid)-1  and c == len(grid[0])-1 and grid[r][c] == 0:
+                return d
+            if grid[r][c] == 1:
+                continue
+            if self.inbounds(grid, r-1, c-1) and (r-1,c-1) not in visited:
+                visited.add((r-1,c-1))
+                queue.append((r-1,c-1,d+1))
+            if self.inbounds(grid, r-1, c) and (r-1,c) not in visited:
+                visited.add((r-1,c))
+                queue.append((r-1,c,d+1))
+            if self.inbounds(grid, r-1, c+1) and (r-1,c+1) not in visited:
+                visited.add((r-1,c+1))
+                queue.append((r-1,c+1,d+1))
+            if self.inbounds(grid, r, c-1) and (r,c-1) not in visited:
+                visited.add((r,c-1))
+                queue.append((r,c-1,d+1))
+            if self.inbounds(grid, r, c+1) and (r,c+1) not in visited:
+                visited.add((r,c+1))
+                queue.append((r,c+1,d+1))
+            if self.inbounds(grid, r+1, c-1) and (r+1,c-1) not in visited:
+                visited.add((r+1,c-1))
+                queue.append((r+1,c-1,d+1))
+            if self.inbounds(grid, r+1, c+1) and (r+1,c+1) not in visited:
+                visited.add((r+1,c+1))
+                queue.append((r+1,c+1,d+1))
+            if self.inbounds(grid, r+1, c,) and (r+1,c) not in visited:
+                visited.add((r+1,c))
+                queue.append((r+1,c,d+1))
+        return -1

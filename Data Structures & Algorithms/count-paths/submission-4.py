86f7d130043b44da6b22c7fe12746e63152c283b
M-Sai-Pranav <94521881+M-Sai-Pranav@users.noreply.github.com>
2026-08-07 12:00:24 -0400

    Add: count-paths - submission-4

---
+++ Data Structures & Algorithms/count-paths/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        def valid(r,c,m,n):
+            if 0 <= r < m and 0 <= c < n:
+                return True 
+            return False 
+        def path(r,c,m,n,memo):
+            if r == m-1 and c == n-1:
+                return 1 
+            if not valid(r,c,m,n):
+                return 0
+            if (r,c) in memo:
+                return memo[(r,c)]
+            down = path(r+1,c,m,n,memo)
+            right = path(r,c+1,m,n,memo)
+            memo[(r,c)] = down+right
+            return memo[(r,c)] 
+        return path(0,0,m,n,{})

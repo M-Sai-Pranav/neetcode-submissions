1a350a7baf46a47d4ccb53dcce043b64443be2c0
M-Sai-Pranav <94521881+M-Sai-Pranav@users.noreply.github.com>
2026-09-03 11:15:36 -0400

    Add: subsets - submission-18

---
+++ Data Structures & Algorithms/subsets/submission-18.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return [[]]
+        
+        first = nums[0]
+        subs_without_first = self.subsets(nums[1:])
+
+        subs_with_first = []
+        for sub in subs_without_first:
+            subs_with_first.append([first, *sub])
+
+        return subs_with_first + subs_without_first 

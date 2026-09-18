7b0dca5ff296559c38856eafce4b9aaf885b3dba
M-Sai-Pranav <94521881+M-Sai-Pranav@users.noreply.github.com>
2026-09-18 11:54:32 -0400

    Add: find-the-town-judge - submission-1

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        num_parents = {}
+        for i in range(1, n+1):
+            num_parents[i] = [0,0]
+        for i,j in trust:
+            num_parents[i][1] += 1 
+            num_parents[j][0] += 1 
+        
+        for ele, degree in num_parents.items():
+            if degree[0] == n-1 and degree[1] == 0:
+                return ele
+        return -1 
